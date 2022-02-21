e9644ba469dc33677a142f3f9e2f023edcdb9aa6
nate2411 <nathandj28@gmail.com>
2022-02-21 13:58:59 +0200

    update

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ const rest = document.getElementById('rest');
 let num = 0;
 count.addEventListener('click', ()=>{
     num++;
+    if(num<10){
+        number.innerText = '0' + num
+    }else{
     number.innerText = num;
-  
+    }   
+})
+
+reset.addEventListener('click', ()=>{
+    num = 0;
+    number.innerText = '00';
 })
